a5ca42dfa245f2919b63a6149a8988c16a1e6bda
syarotokki <maikuratokki@gmail.com>
2025-05-20 23:52:50 +0900

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,5 +1,2 @@
-# utils/checks.py
-DEVELOPER_ID = 1105948117624434728
-
 def is_developer(interaction) -> bool:
-    return interaction.user.id == DEVELOPER_ID
+    return interaction.user.id == 1105948117624434728
